2a38c836ef19e5974081076bf274b857786817d4
曾荣基 <cengrongji@talkmoney.cn>
2018-11-12 18:18:34 +0800

    first commit

---
+++ lwrecyclerview/src/main/java/com/luwei/lwrecyclerview/Adapter/LwAdapter.java
@@ -0,0 +1,14 @@
+package com.luwei.lwrecyclerview.Adapter;
+
+import me.drakeet.multitype.MultiTypeAdapter;
+
+/**
+ * Created by Mr_Zeng
+ *
+ * @date 2018/11/2
+ */
+public class LwAdapter extends MultiTypeAdapter{
+
+
+
+}
